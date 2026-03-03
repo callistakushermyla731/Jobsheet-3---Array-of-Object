07a90978ed85609851f4099a648052f8f66a05e6
Callista Dinar Kushermyla <callistakushermyla731@gmail.com>
2026-03-03 19:34:44 +0700

    Tugas No. 2

---
+++ DosenDemo04.java
@@ -0,0 +1,60 @@
+import java.util.Scanner;
+
+public class DosenDemo04 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Masukkan jumlah dosen: ");
+        int jumlah = sc.nextInt();
+        sc.nextLine(); 
+
+        Dosen04[] daftarDosen = new Dosen04[jumlah];
+
+        for (int i = 0; i < jumlah; i++) {
+            System.out.println("\nData Dosen ke-" + (i + 1));
+
+            System.out.print("Kode          : ");
+            String kode = sc.nextLine();
+
+            System.out.print("Nama          : ");
+            String nama = sc.nextLine();
+
+            System.out.print("Jenis Kelamin (L/P): ");
+            String jkInput = sc.nextLine();
+
+            boolean jenisKelamin;
+            if (jkInput.equalsIgnoreCase("L")) {
+                jenisKelamin = true;
+            } else {
+                jenisKelamin = false;
+            }
+
+            System.out.print("Usia          : ");
+            int usia = sc.nextInt();
+            sc.nextLine(); 
+
+            daftarDosen[i] = new Dosen04(kode, nama, jenisKelamin, usia);
+        }
+
+        System.out.println("\n===== DATA DOSEN =====");
+
+        for (Dosen04 d : daftarDosen) {
+
+            String jk;
+            if (d.jenisKelamin == true) {
+                jk = "Laki-laki";
+            } else {
+                jk = "Perempuan";
+            }
+
+            System.out.println("Kode          : " + d.kode);
+            System.out.println("Nama          : " + d.nama);
+            System.out.println("Jenis Kelamin : " + jk);
+            System.out.println("Usia          : " + d.usia);
+            System.out.println("----------------------------");
+        }
+
+        sc.close();
+    }
+}
