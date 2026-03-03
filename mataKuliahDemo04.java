077e490afcabb1a91141fdc1b3da67f1c8268685
Callista Dinar Kushermyla <callistakushermyla731@gmail.com>
2026-03-03 08:04:23 +0700

    Percobaan 3.4 Langkah 4

--- mataKuliahDemo04.java
+++ mataKuliahDemo04.java
@@ -7,7 +7,7 @@ public class mataKuliahDemo04 {
         int sks, jumlahJam;
 
         for(int i = 0; i < 3; i++) {
-            System.out.println("Masukkan Data Mata Kuliah Ke-" + (i = 1));
+            System.out.println("Masukkan Data Mata Kuliah Ke-" + (i + 1));
             System.out.print("Kode         : ");
             kode = sc.nextLine();
             System.out.print("Nama         : ");
@@ -15,12 +15,21 @@ public class mataKuliahDemo04 {
             System.out.print("SKS          : ");
             dummy = sc.nextLine();
             sks = Integer.parseInt(dummy);
-            System.out.print("Jumlah Jam   :");
+            System.out.print("Jumlah Jam   : ");
             dummy = sc.nextLine();
             jumlahJam = Integer.parseInt(dummy);
             System.out.println("----------------------------------");
 
             arrayOfMataKuliah04[i] = new mataKuliah04(kode, nama, sks, jumlahJam);
+        }
+
+        for(int i = 0; i < 3; i++) {
+            System.out.println("Data Mata Kuliah Ke-" + (i+1));
+            System.out.println("Kode        : " + arrayOfMataKuliah04[i].kode);
+            System.out.println("Nama        : " + arrayOfMataKuliah04[i].nama);
+            System.out.println("SKS         : " + arrayOfMataKuliah04[i].sks);
+            System.out.println("Jumlah Jam  : " + arrayOfMataKuliah04[i].jumlahJam);
+            System.out.println("----------------------------------------");
         }
     }
 }
