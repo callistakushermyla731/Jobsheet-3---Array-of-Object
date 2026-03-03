f135c929a9deb1fb339329e48e1c84c3d23a6d88
Callista Dinar Kushermyla <callistakushermyla731@gmail.com>
2026-03-03 17:28:57 +0700

    Jawaban dari Pertanyaan 3.4.3 No. 4

--- mataKuliahDemo04.java
+++ mataKuliahDemo04.java
@@ -4,14 +4,16 @@ public class mataKuliahDemo04 {
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
-        mataKuliah04[] arrayOfMataKuliah04 = new mataKuliah04[3];
+        System.out.print("Masukkan jumlah mata kuliah: ");
+        int jumlah = sc.nextInt();
+        sc.nextLine();
+
+        mataKuliah04[] arrayOfMataKuliah04 = new mataKuliah04[jumlah];
 
         String kode, nama, dummy;
         int sks, jumlahJam;
 
-        for(int i = 0; i < 3; i++) {
-
-            arrayOfMataKuliah04[i] = new mataKuliah04();
+        for(int i = 0; i < jumlah; i++) {
 
             System.out.println("Masukkan Data Mata Kuliah Ke-" + (i + 1));
 
@@ -31,13 +33,13 @@ public class mataKuliahDemo04 {
 
             System.out.println("----------------------------------");
 
+            arrayOfMataKuliah04[i] = new mataKuliah04();
             arrayOfMataKuliah04[i].tambahData(kode, nama, sks, jumlahJam);
         }
 
         System.out.println("\n===== DATA MATA KULIAH =====");
-        for(int i = 0; i < 3; i++) {
-            System.out.println("Data Mata Kuliah Ke-" + (i+1));
-            arrayOfMataKuliah04[i].cetakInfo();
+        for(mataKuliah04 mk : arrayOfMataKuliah04) {
+            mk.cetakInfo();
         }
 
         sc.close();
